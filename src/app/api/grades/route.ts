ce2bc059f5f1c62a8bd99d293c59a4faee5329bc
Nguyen Hoang Long <longnguyen5112000@gmail.com>
2024-07-19 22:14:18 +0700

    fix 304 status

--- src/app/api/grades/route.ts
+++ src/app/api/grades/route.ts
@@ -2,12 +2,25 @@ import { Grade } from "@/libs/models";
 import { connectToDb } from "@/libs/utils";
 import { NextResponse } from "next/server";
 
-export const GET = async (request: any) => {
+export const GET = async () => {
   try {
     connectToDb();
     const grades = await Grade.find();
-    return NextResponse.json(grades);
+
+    const response = NextResponse.json(grades);
+
+    // Set headers to prevent caching
+    response.headers.set(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Surrogate-Control", "no-store");
+
+    return response;
   } catch (error) {
     console.log(error);
+    return NextResponse.error();
   }
 };
