401084dcc130d335743a8b08fc5ae4cad412b029
Nguyen Hoang Long <longnguyen5112000@gmail.com>
2024-07-19 22:28:11 +0700

    test

--- src/app/api/grades/route.ts
+++ src/app/api/grades/route.ts
@@ -7,20 +7,8 @@ export const GET = async () => {
     connectToDb();
     const grades = await Grade.find();
 
-    const response = NextResponse.json(grades);
-
-    // Set headers to prevent caching
-    response.headers.set(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate"
-    );
-    response.headers.set("Pragma", "no-cache");
-    response.headers.set("Expires", "0");
-    response.headers.set("Surrogate-Control", "no-store");
-
-    return response;
+    return NextResponse.json(grades);
   } catch (error) {
     console.log(error);
-    return NextResponse.error();
   }
 };
