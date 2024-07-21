de28a52a00127a80b71c3d9ad98d5777f0632baf
Nguyen Hoang Long <longnguyen5112000@gmail.com>
2024-07-21 15:32:01 +0700

    fix api grades

--- src/app/api/grades/route.ts
+++ src/app/api/grades/route.ts
@@ -1,28 +1,14 @@
 import { Grade } from "@/libs/models";
 import { connectToDb } from "@/libs/utils";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export const GET = async (request: NextRequest) => {
+export const GET = async () => {
   try {
-    await connectToDb(); // Ensure this is awaited
+    connectToDb();
     const grades = await Grade.find();
 
-    // Create a response and set cache headers to prevent caching
-    const response = NextResponse.json(grades);
-    response.headers.set(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate"
-    );
-    response.headers.set("Pragma", "no-cache");
-    response.headers.set("Expires", "0");
-    response.headers.set("Surrogate-Control", "no-store");
-
-    return response;
+    return Response.json(grades);
   } catch (error) {
     console.error("Error fetching grades:", error);
-    return NextResponse.json(
-      { message: "Failed to fetch grades" },
-      { status: 500 }
-    );
   }
 };
