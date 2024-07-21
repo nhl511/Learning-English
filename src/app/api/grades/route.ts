6ef05ead9ba9c6f5116491e9999de6ec83cc27d9
Nguyen Hoang Long <longnguyen5112000@gmail.com>
2024-07-21 16:00:11 +0700

    fix api grades

--- src/app/api/grades/route.ts
+++ src/app/api/grades/route.ts
@@ -2,12 +2,13 @@ import { Grade } from "@/libs/models";
 import { connectToDb } from "@/libs/utils";
 import { NextResponse } from "next/server";
 
+export const revalidate = 0;
+
 export const GET = async () => {
   try {
     connectToDb();
     const grades = await Grade.find();
-
-    return Response.json(grades);
+    return NextResponse.json(grades);
   } catch (error) {
     console.error("Error fetching grades:", error);
   }
