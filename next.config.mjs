ca74dab96f343df8d30457fb403eb3800e15d293
Nguyen Hoang Long <longnguyen5112000@gmail.com>
2024-09-09 16:22:49 +0700

    fix loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/:path*{/}?",
+        headers: [
+          {
+            key: "X-Accel-Buffering",
+            value: "no",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
