720eddc381d79e98b99b25b119359a9a6153c3a7
Nguyen Hoang Long <longnguyen5112000@gmail.com>
2024-09-16 20:57:50 +0700

    fix

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,6 +2,8 @@ import { User } from "@/libs/models";
 import { connectToDb } from "@/libs/utils";
 import { NextResponse } from "next/server";
 
+export const revalidate = 0;
+
 export const GET = async (request: any, { params }: any) => {
   try {
     connectToDb();
